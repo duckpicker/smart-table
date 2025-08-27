5968586d8a6d59a0c6d8a732e5e3e87b47489b00
duckpicker <mshow353@gmail.com>
2025-08-27 23:03:44 +0300

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+https://github.com/duckpicker/smart-table
 # sp6-2_smart-table_starter
--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,17 @@
 import {rules, createComparison} from "../lib/compare.js";
 
-
-export function initSearching(searchField) {
+export function initSearching(elements, searchField) {
     // @todo: #5.1 — настроить компаратор
+    const comparator = createComparison(
+        [rules.skipEmptyTargetValues],                        // стандартное правило, пропускаем пустые
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)  // ищем в нескольких полях
+    );
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        const query = state[searchField]?.toLowerCase() || '';
+        if (!query) return data;   // если поле поиска пустое — возвращаем все данные
+
+        return data.filter(item => comparator(item, { [searchField]: query }));
     }
 }
