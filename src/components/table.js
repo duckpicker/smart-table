7d91b836d7cd2eb6e0a066039299c19767008f33
duckpicker <mshow353@gmail.com>
2025-09-05 20:00:45 +0300

    fix

--- src/components/table.js
+++ src/components/table.js
@@ -23,8 +23,8 @@ export function initTable(settings, onAction) {
     });
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
-    root.container.addEventListener('change', onAction());
-    root.container.addEventListener('reset',  setTimeout(onAction));
+    root.container.addEventListener('change', () => onAction()); // без () => у меня не рендерится таблица
+    root.container.addEventListener('reset', () => setTimeout(onAction));
     root.container.addEventListener('submit', (e) => {
         e.preventDefault();
         onAction(e.submitter);
